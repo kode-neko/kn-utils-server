fcae5fe07f919ef679cce24bbb6bd35c560a55af
Kodeneko <ladysun.freedom@gmail.com>
2023-05-14 16:08:56 +0200

    [main] cors config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-// import chalk from 'chalk';
 import dotenv from 'dotenv';
 import express, {Express} from 'express';
-// import figlet from 'figlet';
-// import pino from 'express-pino-logger';
 import helmet from 'helmet';
 import cors from 'cors';
 import { rateLimit, RateLimitRequestHandler } from 'express-rate-limit';
@@ -13,18 +10,6 @@ import { createAnimalList } from './animal/Factory';
 
 dotenv.config();
 
-/*
-console.log(
-  chalk.bold.magentaBright(
-    figlet.textSync('Utils Server', {
-      font: 'Cosmike',
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-    }),
-  ),
-);
-*/
-
 createAnimalList();
 
 const app: Express = express();
@@ -32,7 +17,7 @@ const app: Express = express();
 // Config
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors({origin: ["http://www.kodeneko.com", "https://www.kodeneko.com"]}));
 const limiter: RateLimitRequestHandler = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
@@ -42,7 +27,6 @@ const limiter: RateLimitRequestHandler = rateLimit({
 
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
-// app.use(pino.default());
 
 // routes
 app.use('/contact', email);
