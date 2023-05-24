8b775ef794f6e45d9b45ecd96ecf34c43a3589ea
Kodeneko <ladysun.freedom@gmail.com>
2023-05-24 20:57:27 +0200

    [main] hotfix - cors again

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ const app: Express = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors({origin: ["http://www.kodeneko.com", "https://www.kodeneko.com", `localhost:${SERVER_PORT}`]}));
-app.use(cors());
 const limiter: RateLimitRequestHandler = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
